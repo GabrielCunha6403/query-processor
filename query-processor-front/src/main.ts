9127821095d351d694eb096e4454033bed0ea39b
Gabriel Cunha <gabrielcunha2012@gmail.com>
2024-04-23 18:06:07 -0300

    implementação da 'IDE'

--- query-processor-front/src/main.ts
+++ query-processor-front/src/main.ts
@@ -1,11 +1,22 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
+import PrimeVue from 'primevue/config';
 import App from './App.vue'
 import router from './router'
+import Button from 'primevue/button';
+import Editor from 'primevue/editor';
+import Textarea from 'primevue/textarea';
+
+
+import 'primevue/resources/themes/aura-light-green/theme.css'
 
 const app = createApp(App)
 
 app.use(router)
+.use(PrimeVue)
+.component("Button", Button)
+.component("Editor", Editor)
+.component("Textarea", Textarea);
 
 app.mount('#app')
--- query-processor-front/src/views/HomeView.vue
+++ query-processor-front/src/views/HomeView.vue
@@ -1,9 +1,67 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+<template>
+  <div class="script-container">
+    <span>SCRIPT:</span>
+    <Textarea v-model="editor"></Textarea>
+  </div>
+  <Button label="RUN" @click="run()" />
+  <div v-if="output" class="output-container">
+    <span>OUTPUT:</span>
+    {{ output }}
+  </div>
+</template>
+<script lang="ts">
+  
+import { defineComponent } from "vue";
+import Button from "primevue/button";
+import Textarea from "primevue/textarea";
+import Editor from "primevue/editor";
+
+export default defineComponent({
+  name: "QueryProcessor",
+  components: {
+    Button,
+    Editor,
+    Textarea
+  },
+  data() {
+    return {
+      editor: '',
+      output: ''
+    }
+  },
+  methods: {
+    handleCodeEditor(editor: any) {
+      editor.format('Code', true)
+    },
+    run() {
+      this.output = 'oioioioi';
+    }
+  }
+});
+
 </script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+<style scoped>
+textarea {
+  background-color: black;
+  color: rgb(0, 145, 255);
+  font-family: 'Courier New', Courier, monospace;
+  font-weight: bold;
+  width: 1240px !important;
+  min-height: 360px;
+  border: none;
+
+}
+.script-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  margin: 12px 0;
+}
+.output-container {
+  display: flex;
+  flex-direction: column;
+  margin: 12px 0;
+}
+</style>
+
